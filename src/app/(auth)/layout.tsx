9cb3d230aef93e68bae51b25f77b51e4af27dff6
Vighnesh Tirakapadi <vighnesh.tirakapadi@deeporion.com>
2024-12-23 19:41:54 +0530

    completed clerk authentication

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,30 @@
+import { ReactNode } from "react";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Poppins } from "next/font/google";
+import "../globals.css";
+
+export const metadata = {
+  title: "Threads",
+  description: "NextJs Threads App",
+};
+
+interface IRootLayoutProps {
+  children: ReactNode;
+}
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+const RootLayout = ({ children }: IRootLayoutProps) => {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${poppins.className} bg-dark-1`}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+};
+
+export default RootLayout;
