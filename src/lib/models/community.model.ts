842d98937c8d7a75d9130cfbf3bf8e8bdd066a0c
Vighnesh Tirakapadi <vighnesh.tirakapadi@deeporion.com>
2024-12-28 12:02:56 +0530

    communitymodel and webhook

--- src/lib/models/community.model.ts
+++ src/lib/models/community.model.ts
@@ -1,7 +1,24 @@
-import { model } from "mongoose";
-import { models, Schema } from "mongoose";
+import { models, Schema, model } from "mongoose";
 
-const communitySchema = new Schema({});
+const communitySchema = new Schema({
+  id: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  image: String,
+  bio: String,
+  threads: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Thread",
+    },
+  ],
+  members: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
 
 const Community = models.Community || model("Community", communitySchema);
 
